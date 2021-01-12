f317e02bab1d1af8e123308e69f3c6a169cc2b24
RafaelMendes1407 <rafael_mendes@outlook.com>
2021-01-12 16:25:20 -0300

    First Commit

---
+++ src/main/java/br/com/lotoluck/lotoluck/LotoLuckApplication.java
@@ -0,0 +1,13 @@
+package br.com.lotoluck.lotoluck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotoLuckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotoLuckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lotoluck/lotoluck/LotoLuckApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lotoluck.lotoluck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotoLuckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
